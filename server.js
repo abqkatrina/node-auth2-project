b39b225142a200cac152f52297be8171edeceff3
Katrina Hernandez <abqkatrina@gmail.com>
2020-04-21 13:07:58 -0600

    init commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const session = require("express-session");
+
+const userRouter = require("./users/userRouter.js");
+const authRouter = require("./auth/authRouter.js");
+const authenticator = require("./auth/authenticator.js");
+
+const server = express();
+
+const sessionConfig = {
+  name: "biscuit",
+  secret: process.env.SESSION_SECRET || "kore wa himitsu desu",
+  resave: false,
+  saveUninitialized: process.env.SEND_COOKIES || true,
+  cookie: {
+    maxAge: 1000 * 60 * 10, 
+    secure: process.env.USE_SECURE_COOKIES || false, 
+    httpOnly: true
+  },
+};
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+server.use(session(sessionConfig));
+
+server.use("/api/users", authenticator, userRouter);
+server.use("/api/auth", authRouter);
+
+server.get("/", (req, res) => {
+  res.json({ api: "up" });
+});
+
+module.exports = server;
